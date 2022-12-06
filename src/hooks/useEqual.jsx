03f3026ac382e57d9d22ca23d180231d54500a08
eliashz <eliashz@gmail.com>
2022-12-06 11:27:58 +0100

    callback state not working

--- src/hooks/useEqual.jsx
+++ src/hooks/useEqual.jsx
@@ -2,13 +2,13 @@ import { useEffect } from "react"
 
 const useEqual = (setEqual, equal, shape, size, color) => {
     useEffect(() => {
-        setEqual({...equal, [shape]: { size, color }})
+        setEqual(equal => ({...equal, [shape]: { size, color }}))
         if (equal.Triangle.size === equal.Square.size && equal.Square.size === equal.Circle.size) alert("Todas las figuras tienen el mismo tamaño.")
         //console.log(equal, size);
     }, [size])
 
     useEffect(() => {
-        setEqual({...equal, [shape]: { size, color }})
+        setEqual(equal => ({...equal, [shape]: { size, color }}))
         if (equal.Triangle.color === equal.Square.color && equal.Square.color === equal.Circle.color) alert("Todas las figuras tienen el mismo color.")
     }, [color])
 }
