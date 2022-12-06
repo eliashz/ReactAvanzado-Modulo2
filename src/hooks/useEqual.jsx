8f1cac09af25b7316ebdb05c56b4317b61825d95
eliashz <eliashz@gmail.com>
2022-12-06 11:01:11 +0100

    correction alert

--- src/hooks/useEqual.jsx
+++ src/hooks/useEqual.jsx
@@ -4,8 +4,12 @@ const useEqual = (setEqual, equal, shape, size, color) => {
     useEffect(() => {
         setEqual({...equal, [shape]: { size, color }})
         if (equal.Triangle.size === equal.Square.size && equal.Square.size === equal.Circle.size) alert("Todas las figuras tienen el mismo tamaño.")
+    }, [size])
+
+    useEffect(() => {
+        setEqual({...equal, [shape]: { size, color }})
         if (equal.Triangle.color === equal.Square.color && equal.Square.color === equal.Circle.color) alert("Todas las figuras tienen el mismo color.")
-    }, [size, color])
+    }, [color])
 }
 
 export default useEqual;
