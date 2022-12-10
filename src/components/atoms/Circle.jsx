35080115eed30260752fd61aebb1a86f26157e7e
eliashz <eliashz@gmail.com>
2022-12-10 10:58:09 +0100

    Figures separated

---
+++ src/components/atoms/Circle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Circle = () => {
+  return (
+    <svg>
+      
+    </svg>
+  )
+}
+
+export default Circle
---
+++ src/components/atoms/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = () => {
+  return (
+    <svg>
+      
+    </svg>
+  )
+}
+
+export default Square
