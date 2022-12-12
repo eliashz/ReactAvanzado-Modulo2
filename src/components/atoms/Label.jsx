05438a81285667a37dacfe29bff74d3035d8af54
eliashz <eliashz@gmail.com>
2022-12-12 21:35:16 +0100

    2ª revision

--- src/components/atoms/Label.jsx
+++ src/components/atoms/Label.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Label = ({ children }) => {
-  return <label>{children}</label>
+const Label = ({ children, id }) => {
+  return <label htmlFor={id}>{children}</label>
 }
 
 Label.propTypes = {
   children: PropTypes.node.isRequired,
+  id: PropTypes.string.isRequired,
 }
 
 export default Label
--- src/components/molecules/Selector.jsx
+++ src/components/molecules/Selector.jsx
@@ -5,22 +5,18 @@ import PropTypes from 'prop-types'
 const Selector = ({ children, setValue, value }) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Label>
-        {children} Size
-        <Input
-          type='number'
-          style={{ width: '50px' }}
-          min='1'
-          setValue={setValue}
-          valueArr={value}
-          value={value[0]}
-          index={0}
-        ></Input>
-      </Label>
-      <Label>
-        {children} Color
-        <Input type='color' setValue={setValue} valueArr={value} value={value[1]} index={1}></Input>
-      </Label>
+      <Label id='number'>{children} Size</Label>
+      <Input
+        type='number'
+        style={{ width: '50px' }}
+        min='1'
+        setValue={setValue}
+        valueArr={value}
+        value={value[0]}
+        index={0}
+      ></Input>
+      <Label id='color'>{children} Color</Label>
+      <Input type='color' setValue={setValue} valueArr={value} value={value[1]} index={1}></Input>
     </div>
   )
 }
